0a9b63a23efd8da8ec1466faf2ac508b718fcf57
Daniel Pohanka <daniel.pohanka@dynatrace.com>
2016-03-21 11:51:58 -0500

    initial commit

---
+++ test/helloWorld/TestJunit.java
@@ -0,0 +1,17 @@
+package helloWorld;
+import org.junit.Test;
+
+import DemoPrinter.MessageUtil;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestJunit {
+	
+   String message = "Hello World";	
+   MessageUtil messageUtil = new MessageUtil(message);
+
+   @Test
+   public void testPrintMessage() {	  
+      assertEquals(message,messageUtil.printMessage());
+   }
+}
